93f7351521442e6c449c3b2b283c0828698a7fb8
chashmdai <torrejonsotob@gmail.com>
2025-10-16 04:00:28 -0300

    controlador maneja las dos rutas públicas de autenticación

--- src/main/java/com/nexbyte/nexbyteapi/controllers/AuthController.java
+++ src/main/java/com/nexbyte/nexbyteapi/controllers/AuthController.java
@@ -1,20 +1,38 @@
 package com.nexbyte.nexbyteapi.controllers;
 
+import com.nexbyte.nexbyteapi.dto.AuthResponseDTO;
+import com.nexbyte.nexbyteapi.dto.LoginDTO;
+import com.nexbyte.nexbyteapi.dto.RegistroDTO;
 import com.nexbyte.nexbyteapi.services.AuthService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import com.nexbyte.nexbyteapi.dto.RegistroDTO;
 
 @RestController
 @RequestMapping("/api/auth")
 @CrossOrigin(origins = "http://localhost:5173")
 @RequiredArgsConstructor
 public class AuthController {
+
     private final AuthService authService;
+
+    /**
+     * @param registroDTO
+     * @return
+     */
     @PostMapping("/registro")
     public ResponseEntity<String> registrarUsuario(@RequestBody RegistroDTO registroDTO) {
         authService.registrar(registroDTO);
         return ResponseEntity.ok("Usuario registrado con éxito");
+    }
+
+    /**
+     * @param loginDTO
+     * @return
+     */
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponseDTO> login(@RequestBody LoginDTO loginDTO) {
+        AuthResponseDTO response = authService.login(loginDTO);
+        return ResponseEntity.ok(response);
     }
 }
