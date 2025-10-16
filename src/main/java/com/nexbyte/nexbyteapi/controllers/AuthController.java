ffc98d3e2876b3e92c8c2ce2a353f6eb0961a972
chashmdai <torrejonsotob@gmail.com>
2025-10-16 03:34:40 -0300

    controlador usa el servicio por favor 🫸🫷

--- src/main/java/com/nexbyte/nexbyteapi/controllers/AuthController.java
+++ src/main/java/com/nexbyte/nexbyteapi/controllers/AuthController.java
@@ -1,5 +1,7 @@
 package com.nexbyte.nexbyteapi.controllers;
 
+import com.nexbyte.nexbyteapi.services.AuthService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import com.nexbyte.nexbyteapi.dto.RegistroDTO;
@@ -7,11 +9,12 @@ import com.nexbyte.nexbyteapi.dto.RegistroDTO;
 @RestController
 @RequestMapping("/api/auth")
 @CrossOrigin(origins = "http://localhost:5173")
+@RequiredArgsConstructor
 public class AuthController {
-
+    private final AuthService authService;
     @PostMapping("/registro")
-    public ResponseEntity<?> registrarUsuario(@RequestBody RegistroDTO registroDTO) {
-        System.out.println("Datos de registro recibidos: " + registroDTO);
-        return ResponseEntity.ok("Usuario recibido para registro");
+    public ResponseEntity<String> registrarUsuario(@RequestBody RegistroDTO registroDTO) {
+        authService.registrar(registroDTO);
+        return ResponseEntity.ok("Usuario registrado con éxito");
     }
 }
